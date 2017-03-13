59d60ccb13966b41b2df42c57906ad27be2619bb
Peter Roehlen <peter.roehlen@gmail.com>
2017-03-13 14:57:41 +0700

    Table options (in progres)

--- app/src/renderer/components/pages/TablePage.vue
+++ app/src/renderer/components/pages/TablePage.vue
@@ -3,14 +3,30 @@
     <page-header v-bind:title="`Table '${ schema ? schema + '.' : ''}${ table}'`" v-bind:showBack="true"/>
     <div class="header-page-content-top">
       <table class="snapdiff-data-table">
+        <!--Table Options-->
+        <tr>
+          <td v-bind:colspan="columns.length + 1">
+            <label>Rows per page: </label>
+            <input type="number" v-model="limit"/>
+            <snapdiff-pager :totalRecords="totalRows" :recordsPerPage="limit" :currentPage="currentPage"/>
+          </td>
+        </tr>
+        <!--Table Header-->
         <tr>
           <th v-for="column in columns">
             {{ column.name }}
           </th>
         </tr>
+        <!--Table Data-->
         <tr v-for="row in rows">
           <td v-for="column in columns">
             {{ row[column.name] }}
+          </td>
+        </tr>
+        <!--Repeat Pager-->
+        <tr>
+          <td v-bind:colspan="columns.length + 1">
+            <snapdiff-pager :totalRecords="totalRows" :recordsPerPage="limit" :currentPage="currentPage"/>
           </td>
         </tr>
       </table>
@@ -20,24 +36,33 @@
 
 <script>
   import PageHeader from './PageHeader';
+  import SnapdiffPager from '../shared/SnapdiffPager';
 
   export default {
     name: 'table-page',
     props: ['schema', 'table'],
     created() {
       this.getColumns();
-      this.getRows();
+      this.getData();
     },
     components: {
       PageHeader,
+      SnapdiffPager,
     },
     data() {
       return {
-        limit: 25,
+        limit: 2,
         offset: 0,
+        totalRows: 55,
+        currentPage: 1,
         columns: [],
         rows: [],
       };
+    },
+    watch: {
+      limit() {
+        this.getData();
+      },
     },
     methods: {
       getColumns() {
@@ -49,6 +74,13 @@
           .then(results => {
             this.columns = results;
           });
+      },
+      getCount() {
+
+      },
+      getData() {
+        this.getRows();
+        this.getCount();
       },
       getRows() {
         this.$store
---
+++ app/src/renderer/components/shared/SnapdiffPager.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <span>Page {{ currentPage }} of {{ numberPages }}</span>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'snapdiff-pager',
+    props: {
+      totalRecords: {
+        type: Number,
+      },
+      recordsPerPage: {
+        type: Number,
+        default: 10,
+      },
+      currentPage: {
+        type: Number,
+        default: 1,
+      },
+    },
+    computed: {
+      numberPages() {
+        return Math.ceil(this.totalRecords / this.recordsPerPage);
+      },
+    },
+  };
+
+</script>
