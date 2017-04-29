1680f0e74ad6edfd717af9b4e3b1bdc08f8a165c
Peter Roehlen <peter.roehlen@gmail.com>
2017-04-29 11:59:27 +0700

    Show/field values in TableRowPage

--- app/src/renderer/components/TableRowForm.vue
+++ app/src/renderer/components/TableRowForm.vue
@@ -1,8 +1,11 @@
 <template>
-  <span>
-    <h1>Table row as form</h1>
-    {{ row }}
-  </span>
+  <b-table :data="row"
+            :striped="true">
+    <b-table-column field="key"
+                    label="Field" />
+    <b-table-column field="value"
+                    label="Value" />
+  </b-table>
 </template>
 
 <script>
@@ -14,7 +17,8 @@
       },
       row() {
         const rowKey = this.$store.state.tables.current.rowKey;
-        return this.table.rows
+        const fields = this.$store.state.tables.current.columns;
+        const dbRow = this.table.rows
           .find(row => {
             let matched = true;
             const unmatchedKey = this.table.primaryKeyFields
@@ -23,6 +27,13 @@
               matched = false;
             }
             return matched;
+          });
+        return fields
+          .map(field => {
+            const entry = {};
+            entry.key = field.name;
+            entry.value = dbRow[field.name];
+            return entry;
           });
       },
     },
