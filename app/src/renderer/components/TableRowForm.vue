a71514b032a6d7761502a7cc547a569f102231c1
Peter Roehlen <peter.roehlen@gmail.com>
2017-04-29 12:13:09 +0700

    Add snapshot value to TableRowPage

--- app/src/renderer/components/TableRowForm.vue
+++ app/src/renderer/components/TableRowForm.vue
@@ -3,12 +3,16 @@
             :striped="true">
     <b-table-column field="key"
                     label="Field" />
-    <b-table-column field="value"
-                    label="Value" />
+    <b-table-column field="snapshotValue"
+                    :label="table.snapshotCreated ? `@ ${formatTime(table.snapshotCreated)}` : 'No snapshot'" />
+    <b-table-column field="dbValue"
+                    label="Current Value" />
   </b-table>
 </template>
 
 <script>
+  import { formatTime } from '../formatters';
+
   export default {
     name: 'table-row-form',
     computed: {
@@ -16,9 +20,19 @@
         return this.$store.getters['tables/current'];
       },
       row() {
-        const rowKey = this.$store.state.tables.current.rowKey;
-        const fields = this.$store.state.tables.current.columns;
+        const rowKey = this.$store.state.tables.current.rowKey; // TODO get this from this.tables
+        const fields = this.table.columns;
         const dbRow = this.table.rows
+          .find(row => {
+            let matched = true;
+            const unmatchedKey = this.table.primaryKeyFields
+              .find(field => rowKey[field] !== row[field]);
+            if (unmatchedKey) {
+              matched = false;
+            }
+            return matched;
+          });
+        const snapshotRow = !this.table.snapshotCreated ? {} : this.table.snapshot
           .find(row => {
             let matched = true;
             const unmatchedKey = this.table.primaryKeyFields
@@ -32,12 +46,14 @@
           .map(field => {
             const entry = {};
             entry.key = field.name;
-            entry.value = dbRow[field.name];
+            entry.dbValue = dbRow[field.name];
+            entry.snapshotValue = snapshotRow[field.name];
             return entry;
           });
       },
     },
     methods: {
+      formatTime,
     },
   };
 
