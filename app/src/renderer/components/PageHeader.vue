cd301832665b51399b1a06298893e01db86f4da6
Peter Roehlen <peter.roehlen@gmail.com>
2017-05-02 12:57:48 +0700

    Possible fix issue #13

--- app/src/renderer/components/PageHeader.vue
+++ app/src/renderer/components/PageHeader.vue
@@ -1,29 +1,33 @@
 <template>
   <nav v-bind:class="{
-    nav: true,
-    'has-shadow': !noHome
-   }">
-    <div class="container">
-      <div v-if="!noHome" class="nav-left">
-        <b-tooltip label="Sign-out" position="is-bottom">
-          <a class="nav-item" v-on:click="goHome">
-            <b-icon icon="home"/>
-            <img src="../../../../art/banner_logo_nav.png"/>
-          </a>
-        </b-tooltip>
-      </div>
-      
-      <div class="nav-center">
-        <strong class="nav-item" v-if="title">{{ title }}</strong>
-      </div>
-
-      <div class="nav-right">
-        <b-tooltip label="Quit" position="is-bottom">
-          <a class="nav-item" v-on:click="quit">
-            <b-icon icon="power_settings_new"/>
-          </a>
-        </b-tooltip>
-      </div>
+      nav: true,
+      'has-shadow': !noHome
+     }">
+    <div v-if="!noHome"
+         class="nav-left">
+      <b-tooltip label="Sign-out"
+                 position="is-bottom">
+        <a class="nav-item"
+           v-on:click="goHome">
+          <b-icon icon="home" />
+          <img src="../../../../art/banner_logo_nav.png" />
+        </a>
+      </b-tooltip>
+    </div>
+  
+    <div class="nav-center">
+      <strong class="nav-item"
+              v-if="title">{{ title }}</strong>
+    </div>
+  
+    <div class="nav-right">
+      <b-tooltip label="Quit"
+                 position="is-bottom">
+        <a class="nav-item"
+           v-on:click="quit">
+          <b-icon icon="power_settings_new" />
+        </a>
+      </b-tooltip>
     </div>
   </nav>
 </template>
