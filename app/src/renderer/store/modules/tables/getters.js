b39c0a3bd82ae76e4c2b23700759cfdad152e144
Peter Roehlen <peter.roehlen@gmail.com>
2017-05-03 11:46:03 +0700

    Start refactoring Table-related views/routes into single page with tabs

--- app/src/renderer/store/modules/tables/getters.js
+++ app/src/renderer/store/modules/tables/getters.js
@@ -5,4 +5,7 @@ export default {
     const table = state.all[state.current.index];
     return Object.assign(table, state.current);
   },
+  tablePageCount(state) {
+    return Math.ceil(state.current.totalRows / state.current.rowsPerPage);
+  },
 };
